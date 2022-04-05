d37b5bb2fe5d410a27fb7898c9d0c8b07c5e67e9
mslalith <santosh2397@gmail.com>
2022-04-05 23:33:19 +0530

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,11 @@
+# telescope.nvim dependencies
+brew install ripgrep
+brew install fd
+
+# Support for Icons & Font
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-hack-nerd-font
+
+# Install LSP language servers
+# :LspInstall <language>
+
