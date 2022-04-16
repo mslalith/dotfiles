50bd6dbad5927f8e2629cec80619c3786b6bb402
mslalith <santosh2397@gmail.com>
2022-04-16 16:51:55 +0530

    change quotes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,15 @@
-require('plugins')
-require('options')
-require('keymaps')
-require('colorscheme')
+require("plugins")
+require("options")
+require("keymaps")
+require("colorscheme")
 
 -- Plugins
 require("plugins/autopairs")
-require('plugins/lualine')
-require('plugins/telescope')
-require('plugins/nvim-web-devicons')
-require('plugins/completion')
-require('plugins/treesitter')
+require("plugins/lualine")
+require("plugins/telescope")
+require("plugins/nvim-web-devicons")
+require("plugins/completion")
+require("plugins/treesitter")
 
 -- LSP
 require("plugins.lsp.lsp-installer")
