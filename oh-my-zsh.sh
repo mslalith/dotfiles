59d0418b482c4cde61e2d1c90a57de0aa56f802f
mslalith <mslalith.opensource@gmail.com>
2022-07-10 18:55:40 +0530

    change path of oh-my-zsh theme

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -2,7 +2,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # copy theme
-cp ./zsh/.oh-my-zsh/themes/mslalith.zsh-theme $ZSH/themes/mslalith.zsh-theme
+cp ./files/.oh-my-zsh/themes/mslalith.zsh-theme $ZSH/themes/mslalith.zsh-theme
 
 # install plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
