075f1083fcdada7be500340a8597af20e10dd380
mslalith <mslalith.opensource@gmail.com>
2022-07-23 18:38:58 +0530

    migrate nvim-tree global variables to options

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,53 +1,5 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
-vim.g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-vim.g.nvim_tree_group_empty = 1 --  0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-vim.g.nvim_tree_symlink_arrow = " >> " --  defaults to " ➛ ". used as a separator between symlinks' source and target.
-vim.g.nvim_tree_respect_buf_cwd = 1 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-vim.g.nvim_tree_create_in_closed_folder = 1 -- 0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
--- vim.g.nvim_tree_respect_buf_cwd = 1
-vim.g.nvim_tree_special_files = {
-  ["Cargo.toml"] = true,
-  Makefile = true,
-  MAKEFILE = 1,
-  ["README.md"] = true,
-  ["readme.md"] = true,
-} --  List of filenames that gets highlighted with NvimTreeSpecialFile
-vim.g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1,
-  folder_arrows = 1,
-}
-
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "✗",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "★",
-    deleted = "",
-    ignored = "◌"
-  },
-  folder = {
-    arrow_open = "",
-    arrow_closed = "",
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-    symlink_open = ""
-  }
-}
-
 local nvim_tree = require("nvim-tree")
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 local M = {}
@@ -60,6 +12,8 @@ function M.config()
     hijack_cursor = false,
     hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
+    create_in_closed_folder = true,
+    respect_buf_cwd = true,
     ignore_buffer_on_setup = false,
     open_on_setup = false,
     open_on_tab = false,
@@ -153,8 +107,53 @@ function M.config()
       },
     },
     renderer = {
+      root_folder_modifier = ":~",
+      highlight_git = true,
+      add_trailing = true,
+      group_empty = true,
+      highlight_opened_files = "icon",
       indent_markers = {
         enable = true,
+      },
+      special_files = {
+        ["Cargo.toml"] = true,
+        Makefile = true,
+        MAKEFILE = 1,
+        ["README.md"] = true,
+        ["readme.md"] = true,
+      },
+      icons = {
+        padding = " ",
+        symlink_arrow = " >> ",
+        show = {
+          git = true,
+          folder = true,
+          file = true,
+          folder_arrow = true,
+        },
+        glyphs = {
+          default = "",
+          symlink = "",
+          git = {
+            unstaged = "✗",
+            staged = "✓",
+            unmerged = "",
+            renamed = "➜",
+            untracked = "★",
+            deleted = "",
+            ignored = "◌"
+          },
+          folder = {
+            arrow_open = "",
+            arrow_closed = "",
+            default = "",
+            open = "",
+            empty = "",
+            empty_open = "",
+            symlink = "",
+            symlink_open = ""
+          }
+        }
       }
     }
   }
