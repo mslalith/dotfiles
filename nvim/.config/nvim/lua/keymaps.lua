aba126d456e9df8754388ddf78439673cb34f4e6
mslalith <mslalith.opensource@gmail.com>
2022-08-07 15:26:53 +0530

    nvim - add useful key remaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -15,7 +15,6 @@ keymap("", "<Space>", "<Nop>", opts)
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Normal --
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
@@ -23,6 +22,7 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 keymap("n", "<F5>", ":so %<CR>", opts)
+keymap("n", "<C-s>", ":w<CR>", opts)
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Resize with arrows
@@ -48,28 +48,26 @@ keymap("n", "<leader><leader>b", "<cmd>Telescope buffers<CR>", opts)
 keymap("n", "<leader><leader>s", "<cmd>Telescope live_grep<CR>", opts)
 keymap("n", "<leader><leader>h", "<cmd>Telescope help_tags<CR>", opts)
 
--- Insert --
 -- Press jk fast to enter Normal mode
 keymap("i", "jk", "<ESC>", opts)
 
--- Visual --
 -- Stay in indent mode
-keymap("v", "<", "<gv", opts)
-keymap("v", ">", ">gv", opts)
+keymap("n", "<Tab>", "v>gv<Esc>", opts)
+keymap("n", "<S-Tab>", "v<gv<Esc>", opts)
+keymap("v", "<Tab>", ">gv", opts)
+keymap("v", "<S-Tab>", "<gv", opts)
 
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
--- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal --
 -- Better terminal navigation
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
