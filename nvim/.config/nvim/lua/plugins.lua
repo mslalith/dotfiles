7f613e6235a6663d1952ec93a92999fcbf662f16
mslalith <mslalith.opensource@gmail.com>
2022-08-22 19:13:02 +0530

    nvim - format plugins.lua

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,147 +1,148 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    packer_bootstrap =
+        fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
 end
 
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  vim.notify('packer not found !!!')
-  return
+    vim.notify("packer not found !!!")
+    return
 end
 
 -- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
 }
 
 local packer_startup = packer.startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-  -- General
-  use 'windwp/nvim-autopairs'
-  use 'rcarriga/nvim-notify'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use "akinsho/toggleterm.nvim"
-  use 'stevearc/aerial.nvim'
-  use 'rmagatti/auto-session'
-  use "xiyaowong/nvim-transparent"
+    -- General
+    use("windwp/nvim-autopairs")
+    use("rcarriga/nvim-notify")
+    use {
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    }
+    use("akinsho/toggleterm.nvim")
+    use("stevearc/aerial.nvim")
+    use("rmagatti/auto-session")
+    use("xiyaowong/nvim-transparent")
 
-  -- Bufferline
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "*",
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  use "moll/vim-bbye"
+    -- Bufferline
+    use {
+        "akinsho/bufferline.nvim",
+        tag = "*",
+        requires = "kyazdani42/nvim-web-devicons",
+    }
+    use("moll/vim-bbye")
 
-  -- Better Comments
-  use 'numToStr/Comment.nvim'
+    -- Better Comments
+    use("numToStr/Comment.nvim")
 
-  -- Trouble
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-  }
+    -- Trouble
+    use {
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+    }
 
-  -- Hop
-  use {
-    'phaazon/hop.nvim',
-    branch = 'v2', -- optional but strongly recommended
-  }
+    -- Hop
+    use {
+        "phaazon/hop.nvim",
+        branch = "v2", -- optional but strongly recommended
+    }
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use 'nvim-telescope/telescope-media-files.nvim'
-  use {
-    "AckslD/nvim-neoclip.lua",
-    requires = { { 'nvim-telescope/telescope.nvim' } },
-  }
+    -- Telescope
+    use {
+        "nvim-telescope/telescope.nvim",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    }
+    use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+    use("nvim-telescope/telescope-media-files.nvim")
+    use {
+        "AckslD/nvim-neoclip.lua",
+        requires = { { "nvim-telescope/telescope.nvim" } },
+    }
 
-  -- nvim-tree
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  }
+    -- nvim-tree
+    use {
+        "kyazdani42/nvim-tree.lua",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    }
 
-  -- Completion
-  use "hrsh7th/nvim-cmp"
-  use "onsails/lspkind-nvim"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-  use "quangnguyen30192/cmp-nvim-ultisnips"
-  use "L3MON4D3/LuaSnip"
-  use "saadparwaiz1/cmp_luasnip"
-  use "rafamadriz/friendly-snippets"
+    -- Completion
+    use("hrsh7th/nvim-cmp")
+    use("onsails/lspkind-nvim")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-nvim-lua")
+    use("quangnguyen30192/cmp-nvim-ultisnips")
+    use("L3MON4D3/LuaSnip")
+    use("saadparwaiz1/cmp_luasnip")
+    use("rafamadriz/friendly-snippets")
 
-  -- Native LSP
-  use 'neovim/nvim-lspconfig'
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
+    -- Native LSP
+    use("neovim/nvim-lspconfig")
+    use("williamboman/mason.nvim")
+    use("williamboman/mason-lspconfig.nvim")
 
-  --
-  use {
-    "jose-elias-alvarez/null-ls.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-  }
+    --
+    use {
+        "jose-elias-alvarez/null-ls.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    }
 
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  }
+    -- Treesitter
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
 
-  -- Git
-  use {
-    'lewis6991/gitsigns.nvim',
-    tag = 'release' -- To use the latest release
-  }
-  use {
-    'sindrets/diffview.nvim',
-    requires = 'nvim-lua/plenary.nvim'
-  }
+    -- Git
+    use {
+        "lewis6991/gitsigns.nvim",
+        tag = "release", -- To use the latest release
+    }
+    use {
+        "sindrets/diffview.nvim",
+        requires = "nvim-lua/plenary.nvim",
+    }
 
-  -- Themes
-  use "projekt0n/github-nvim-theme"
-  use 'folke/tokyonight.nvim'
-  use "EdenEast/nightfox.nvim"
-  use {
-    "catppuccin/nvim",
-    as = "catppuccin",
-    run = ":CatppuccinCompile"
-  }
+    -- Themes
+    use("projekt0n/github-nvim-theme")
+    use("folke/tokyonight.nvim")
+    use("EdenEast/nightfox.nvim")
+    use {
+        "catppuccin/nvim",
+        as = "catppuccin",
+        run = ":CatppuccinCompile",
+    }
 
-  -- Icons
-  use 'ryanoasis/vim-devicons'
+    -- Icons
+    use("ryanoasis/vim-devicons")
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require("packer").sync()
+    end
 end)
 
 return packer_startup
