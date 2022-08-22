5cca5c502aedcbfe01d42a4d072ec1117e7f7fdf
mslalith <mslalith.opensource@gmail.com>
2022-08-22 12:22:38 +0530

    nvim - refactor tsserver lsp config

--- nvim/.config/nvim/lua/plugins/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-installer.lua
@@ -20,5 +20,12 @@ local opts = {
 local sumneko_opts = require("plugins.lsp.settings.sumneko_lua")
 lspconfig.sumneko_lua.setup(vim.tbl_deep_extend("force", sumneko_opts, opts))
 
-lspconfig.tsserver.setup(opts)
-
+lspconfig.tsserver.setup({
+  on_attach = function(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+    opts.on_attach(client, bufnr)
+  end,
+  capabilities = opts.capabilities
+})
