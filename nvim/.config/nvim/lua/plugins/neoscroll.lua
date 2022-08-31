1c20d20c2f568e5a057e180f4139328b41939c9e
mslalith <mslalith.opensource@gmail.com>
2022-08-31 11:48:13 +0530

    nvim - add new keymaps to neoscroll

--- nvim/.config/nvim/lua/plugins/neoscroll.lua
+++ nvim/.config/nvim/lua/plugins/neoscroll.lua
@@ -1,1 +1,9 @@
-require("neoscroll").setup {}
+require("neoscroll").setup {
+    mappings = { "<C-u>", "<C-d>", "zt", "zz", "zb" },
+    easing_function = "quadratic",
+}
+
+require("neoscroll.config").set_mappings {
+    ["<A-j>"] = { "scroll", { "vim.wo.scroll", "true", "350" } },
+    ["<A-k>"] = { "scroll", { "-vim.wo.scroll", "true", "350" } },
+}
