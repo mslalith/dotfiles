599dffe663f194c0ccc5ca3ca6f98f1a2d60ee05
mslalith <mslalith.opensource@gmail.com>
2022-10-09 12:37:44 +0530

    nvim - add ms_config global var

---
+++ nvim/.config/nvim/lua/ms_config.lua
@@ -0,0 +1,13 @@
+ms_config = {
+    plugins = {
+        dressing = {
+            disabled = true,
+        },
+        lsp_lines = {
+            disabled = true,
+        },
+        treesitter_context = {
+            disabled = false,
+        },
+    },
+}
