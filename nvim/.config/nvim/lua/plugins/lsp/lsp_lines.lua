5b2541a020a5584b7141f98fb9e122927ac40618
mslalith <mslalith.opensource@gmail.com>
2022-10-09 12:36:12 +0530

    nvim - refactor lsp_lines.nvim

--- nvim/.config/nvim/lua/plugins/lsp/lsp_lines.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp_lines.lua
@@ -2,35 +2,13 @@ local lsp_lines = require("lsp_lines")
 
 local M = {}
 
-local function enable()
-    -- disable virtual_text if lsp_lines is enabled
-    lsp_lines.toggle()
-    vim.diagnostic.config {
-        virtual_text = false,
-    }
-end
-
-local function disable()
-    -- enable virtual_text if lsp_lines is disabled
-    lsp_lines.toggle()
-    vim.diagnostic.config {
-        virtual_text = true,
-    }
-end
-
 function M.toggle()
-    -- lsp_lines doesn't have any API to derive if it's enabled or not
-    -- we depened on virtual_text
-    local is_enabled = not vim.diagnostic.config().virtual_text
-    if is_enabled then
-        disable()
-    else
-        enable()
-    end
+    vim.diagnostic.config {
+        virtual_text = not lsp_lines.toggle(),
+    }
 end
 
--- enable lsp_lines to being with
-enable()
+M.toggle()
 lsp_lines.setup()
 
 return M
