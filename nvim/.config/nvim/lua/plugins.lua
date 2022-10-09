0325162e19995efe44be0b0ab0d105a417487942
mslalith <mslalith.opensource@gmail.com>
2022-10-09 12:49:21 +0530

    nvim - refactor packer initialization

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,24 +1,24 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap =
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
         fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
 end
 
+local packer_bootstrap = ensure_packer()
+local packer = require("packer")
+
+-- run PackerSync on save
 vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-    vim.notify("packer not found !!!")
-    return
-end
 
 -- Have packer use a popup window
 packer.init {
