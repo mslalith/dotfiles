bd84615735ddcb67226cef05dac49260a05c3d6d
mslalith <mslalith.opensource@gmail.com>
2022-10-12 15:45:20 +0530

    nvim: move init.lua loading to /core/init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,3 @@
 require("impatient").enable_profile()
 
-require("ms_config")
-
-ms_config.global("mapleader", " ")
-ms_config.keys.insert_mode("jk", "<ESC>")
-ms_config.disable_distribution_plugins()
-
-require("plugins")
-require("options")
-require("keymaps")
-require("colorscheme")
-require("plugins.lsp.commands")
+require("core")
---
+++ nvim/.config/nvim/lua/core/init.lua
@@ -0,0 +1,11 @@
+require("ms_config")
+
+ms_config.global("mapleader", " ")
+ms_config.keys.insert_mode("jk", "<ESC>")
+ms_config.disable_distribution_plugins()
+
+require("plugins")
+require("options")
+require("keymaps")
+require("colorscheme")
+require("plugins.lsp.commands")
