6de095208524ccf7a24259b433f8a6d1924bfcc6
mslalith <mslalith.opensource@gmail.com>
2022-12-26 07:17:56 +0530

    nvim: remove unused plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,21 +1,4 @@
 return {
     "nvim-lua/plenary.nvim",
     "ryanoasis/vim-devicons",
-
-    -- UI
-    -- {
-    --     "stevearc/dressing.nvim",
-    --     disable = ms.plugins.dressing.disabled,
-    --     config = function()
-    --         require("plugins.dressing")
-    --     end,
-    -- },
-    -- {
-    --     "gelguy/wilder.nvim",
-    --     event = { "CmdwinEnter", "CmdlineEnter" },
-    --     disable = ms.plugins.wilder.disabled,
-    --     config = function()
-    --         require("plugins.wilder")
-    --     end,
-    -- }, -- adventurous wildmenu
 }
