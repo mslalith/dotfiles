55f84987aba795b8265b6eaca3f7a92441903fbb
= <=>
2025-02-06 11:11:04 +0530

    nvim: reconfigure aerial

--- roles/neovim/files/lua/plugins/aerial.lua
+++ roles/neovim/files/lua/plugins/aerial.lua
@@ -1,11 +1,27 @@
 local M = {
     "stevearc/aerial.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
 }
 
 function M.config()
+    ms.keys.normal_mode("<leader>a", "<cmd>AerialToggle<CR>")
+
     require("aerial").setup {
+        on_attach = function(bufnr)
+            ms.keys.normal_mode("m", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+            ms.keys.normal_mode("M", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+        close_on_select = true,
         layout = {
             min_width = 40,
+            default_direction = "float",
+        },
+        float = {
+            relative = "win",
         },
     }
 end
