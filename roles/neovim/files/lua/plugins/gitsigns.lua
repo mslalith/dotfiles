76c619cbb6966c92d6dac67c1e66553972ea6d8b
= <=>
2025-02-07 13:28:34 +0530

    nvim: update gitsigns keymaps

--- roles/neovim/files/lua/plugins/gitsigns.lua
+++ roles/neovim/files/lua/plugins/gitsigns.lua
@@ -7,12 +7,59 @@ local M = {
 function M.config()
     require("gitsigns").setup {
         on_attach = function(bufnr)
+            local gitsigns = require("gitsigns")
+
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            --------------------------------------
             -- Navigation
-            ms.keys.normal_mode("hj", ":Gitsigns next_hunk<CR>")
-            ms.keys.normal_mode("hk", ":Gitsigns prev_hunk<CR>")
-            ms.keys.normal_mode("hp", ":Gitsigns preview_hunk<CR>")
-            ms.keys.normal_mode("hd", ":Gitsigns diffthis<CR>")
-            ms.keys.normal_mode("hb", ":Gitsigns blame_line<CR>")
+            --------------------------------------
+            map("n", "hj", function()
+                if vim.wo.diff then
+                    vim.cmd.normal { "hj", bang = true }
+                else
+                    gitsigns.nav_hunk("next")
+                end
+            end)
+
+            map("n", "hk", function()
+                if vim.wo.diff then
+                    vim.cmd.normal { "hk", bang = true }
+                else
+                    gitsigns.nav_hunk("prev")
+                end
+            end)
+
+            --------------------------------------
+            -- Actions
+            --------------------------------------
+            map("n", "<leader>hs", gitsigns.stage_hunk)
+            map("n", "<leader>hr", gitsigns.reset_hunk)
+
+            map("v", "<leader>hs", function()
+                gitsigns.stage_hunk { vim.fn.line("."), vim.fn.line("v") }
+            end)
+
+            map("v", "<leader>hr", function()
+                gitsigns.reset_hunk { vim.fn.line("."), vim.fn.line("v") }
+            end)
+
+            map("n", "<leader>hS", gitsigns.stage_buffer)
+            map("n", "<leader>hR", gitsigns.reset_buffer)
+            map("n", "<leader>hp", gitsigns.preview_hunk)
+            map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+            map("n", "<leader>hb", function()
+                gitsigns.blame_line { full = true }
+            end)
+
+            map("n", "<leader>hd", function()
+                gitsigns.diffthis("~")
+            end)
         end,
 
         signs = {
