7d18b6c904ca09ba95908dd6793d34afdbc756ca
= <=>
2025-02-07 15:46:25 +0530

    nvim: restructure lazy setup

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,16 +1,7 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    }
-end
-vim.opt.runtimepath:prepend(lazypath)
+require("ms_config")
+ms.global("mapleader", " ")
+ms.keys.insert_mode("jk", "<ESC>")
+ms.disable_distribution_plugins()
 
 require("ms_config")
 ms.global("mapleader", " ")
@@ -18,14 +9,7 @@ ms.keys.insert_mode("jk", "<ESC>")
 ms.disable_distribution_plugins()
 
 require("options")
-require("lazy").setup {
-    spec = {
-        { import = "plugins" },
-        { import = "plugins.colorscheme" },
-    },
-    defaults = { lazy = true },
-    ui = { border = "rounded" },
-}
+require("config.lazy")
 
 require("keymaps")
 require("plugins.lsp.commands")
