9693d42b3f16d557001c94b84b8e168f1e97c19d
mslalith <mslalith.opensource@gmail.com>
2025-02-08 08:13:44 +0530

    nvim: restructure

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -4,7 +4,7 @@ ms.keys.insert_mode("jk", "<ESC>", "Goto normal mode")
 ms.disable_distribution_plugins()
 
 require("options")
-require("config.lazy")
+require("config")
 
 require("keymaps")
 require("plugins.lsp.commands")
