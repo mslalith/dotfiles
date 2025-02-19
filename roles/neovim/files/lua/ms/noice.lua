dfecb73842c0aaa9ba695d44946812570dcc4ee7
mslalith <mslalith.opensource@gmail.com>
2025-02-19 11:12:35 +0530

    nvim: update noice.nvim config

--- roles/neovim/files/lua/ms/noice.lua
+++ roles/neovim/files/lua/ms/noice.lua
@@ -7,6 +7,7 @@ local M = {
 }
 
 function M.config()
+    ---@diagnostic disable-next-line: missing-fields
     require("noice").setup {
         routes = {
             {
@@ -22,51 +23,22 @@ function M.config()
                 opts = { skip = true },
             },
         },
-        lsp_progress = {
-            enabled = false,
-        },
-        popupmenu = {
-            enabled = true,
-            backend = "cmp",
-        },
-        cmdline = {
-            enabled = true,
-            menu = "popup",
-            icons = {
-                ["/"] = { icon = "/", hl_group = "DiagnosticWarn" },
-                ["?"] = { icon = "?", hl_group = "DiagnosticWarn" },
-                [":"] = { icon = ":", hl_group = "DiagnosticInfo", firstc = false },
+        lsp = {
+            progress = {
+                enabled = false,
+            },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
             },
         },
-        views = {
-            cmdline_popup = {
-                position = {
-                    row = 10,
-                    col = "50%",
-                },
-                size = {
-                    width = 60,
-                    height = "auto",
-                },
-            },
-            popupmenu = {
-                relative = "editor",
-                position = {
-                    row = 8,
-                    col = "50%",
-                },
-                size = {
-                    width = 60,
-                    height = 10,
-                },
-                border = {
-                    style = "rounded",
-                    padding = { 0, 1 },
-                },
-                win_options = {
-                    winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-                },
-            },
+        presets = {
+            bottom_search = false, -- use a classic bottom cmdline for search
+            command_palette = true, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true, -- add a border to hover docs and signature help
         },
     }
 end
