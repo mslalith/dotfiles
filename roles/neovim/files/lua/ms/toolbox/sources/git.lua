0f22c4fe1106f2904f8b92e30bbebdcfb8c185b2
mslalith <mslalith.opensource@gmail.com>
2025-02-22 12:33:37 +0530

    nvim: create toolbox source and refactor git

--- roles/neovim/files/lua/ms/toolbox/sources/git.lua
+++ roles/neovim/files/lua/ms/toolbox/sources/git.lua
@@ -1,62 +1,42 @@
----@class ms.toolbox.sources.git
-local M = {}
-
----@param key string
----@param title string
----@param message string
----@return FidgetNotifier
-local function get_or_create_task(key, title, message)
-    return require("ms.extras.fidget.notifier").get_or_create_task(key, "Git", title, message)
-end
-
----@param cmd string[]
----@param key string
----@param title string
----@param message string
----@param failure? fun(obj: vim.SystemCompleted): boolean
-local function run_task_if_required(cmd, key, title, message, failure)
-    if not key then
-        require("ms.toolbox").notify_error("key is required")
-    end
-    if not title then
-        require("ms.toolbox").notify_error("title is required")
-    end
-
-    local task = get_or_create_task(key, title, message)
-    if task.is_running then
-        return
-    end
-
-    failure = failure or function(obj)
-        return obj.code ~= 0
-    end
-
-    vim.system(cmd, { text = true }, function(obj)
-        task:finish()
-        if failure(obj) then
-            require("ms.toolbox").notify_error(title .. " failed")
-        end
-    end)
-end
+---@class ms.toolbox.sources.git : ms.toolbox.source.Source
+local M = require("ms.toolbox.sources.source"):new()
 
 ---@type ms.toolbox.Command[]
 M.cmds = {
     {
         name = "Git Fetch",
         execute = function()
-            run_task_if_required({ "git", "fetch" }, "git_fetch", "Git Fetch", "fetching")
+            M:run_task {
+                cmd = { "git", "fetch" },
+                group = "Git",
+                key = "git_fetch",
+                title = "Git Fetch",
+                message = "fetching",
+            }
         end,
     },
     {
         name = "Git Pull",
         execute = function()
-            run_task_if_required({ "git", "pull" }, "git_pull", "Git Pull", "pulling")
+            M:run_task {
+                cmd = { "git", "pull" },
+                group = "Git",
+                key = "git_pull",
+                title = "Git Pull",
+                message = "pulling",
+            }
         end,
     },
     {
         name = "Git Push",
         execute = function()
-            run_task_if_required({ "git", "push" }, "git_push", "Git Push", "pushing")
+            M:run_task {
+                cmd = { "git", "push" },
+                group = "Git",
+                key = "git_push",
+                title = "Git Push",
+                message = "pushing",
+            }
         end,
     },
 }
