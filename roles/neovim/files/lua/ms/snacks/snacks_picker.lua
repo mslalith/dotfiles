0e83e7d1b5377f441059374e39df01c09a6335c4
mslalith <mslalith.opensource@gmail.com>
2025-03-25 12:51:01 +0530

    nvim: update snacks pickers

--- roles/neovim/files/lua/ms/snacks/snacks_picker.lua
+++ roles/neovim/files/lua/ms/snacks/snacks_picker.lua
@@ -24,7 +24,6 @@ local M = {
         { "<leader>e", "<cmd>lua require('snacks').explorer()<cr>", desc = "File Explorer" },
         { "<leader>/", "<cmd>lua require('snacks').picker.grep()<cr>", desc = "Grep" },
         { "<A-S-f>", "<cmd>lua require('snacks').picker.grep()<cr>", desc = "Grep" },
-        { "<leader>sg", "<cmd>lua require('snacks').picker.grep()<cr>", desc = "Grep" },
         { "<leader>sw", "<cmd>lua require('snacks').picker.grep_word()<cr>", desc = "Grep selected word" },
         { "<leader>sr", "<cmd>lua require('snacks').picker.resume()<cr>", desc = "Resume last picker" },
         {
@@ -57,7 +56,10 @@ local M = {
         {
             "<leader>ff",
             function()
-                Snacks.picker.smart { layout = MsConfig.snacks.layouts.vscode_bordered }
+                Snacks.picker.smart {
+                    multi = { "buffers", "files" },
+                    layout = MsConfig.snacks.layouts.vscode_bordered,
+                }
             end,
             desc = "Find Files (smart)",
         },
@@ -66,6 +68,7 @@ local M = {
             "<leader>ff",
             function()
                 Snacks.picker.smart {
+                    multi = { "buffers", "files" },
                     layout = MsConfig.snacks.layouts.vscode_bordered,
                     search = function(picker)
                         return picker:word()
@@ -75,11 +78,42 @@ local M = {
             desc = "Find Files (smart)",
         },
         {
+            "<leader>fr",
+            function()
+                Snacks.picker.recent { layout = MsConfig.snacks.layouts.vscode_bordered }
+            end,
+            desc = "Buffers",
+        },
+        {
             "<leader>fb",
             function()
                 Snacks.picker.buffers { layout = MsConfig.snacks.layouts.vscode_bordered }
             end,
             desc = "Buffers",
+        },
+        {
+            mode = "n",
+            "<leader>sg",
+            function()
+                Snacks.picker.grep()
+            end,
+            desc = "Grep",
+        },
+        {
+            mode = "n",
+            "<leader>sG",
+            function()
+                Snacks.picker.grep_word()
+            end,
+            desc = "Grep",
+        },
+        {
+            mode = "v",
+            "<leader>sg",
+            function()
+                Snacks.picker.grep_word()
+            end,
+            desc = "Grep",
         },
     },
 }
