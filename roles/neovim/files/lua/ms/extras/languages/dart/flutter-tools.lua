0d8854023b221fb012d470c59aa5a36f9f6f1908
mslalith <mslalith.opensource@gmail.com>
2025-04-04 17:50:00 +0530

    nvim: add flutter plugin commands

--- roles/neovim/files/lua/ms/extras/languages/dart/flutter-tools.lua
+++ roles/neovim/files/lua/ms/extras/languages/dart/flutter-tools.lua
@@ -47,6 +47,68 @@ function M.config()
             end,
         },
     }
+
+    Toolbox.plugins.add_plugin_commands {
+        name = "Flutter",
+        commands = {
+            {
+                name = "Run app",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterRun")
+                end,
+            },
+            {
+                name = "Run app (Web)",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterRun -d chrome")
+                end,
+            },
+            {
+                name = "Stop app",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterQuit")
+                end,
+            },
+            {
+                name = "Pub Get",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterPubGet")
+                end,
+            },
+            {
+                name = "Restart app",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterRestart")
+                end,
+            },
+            {
+                name = "Reload app",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterReload")
+                end,
+            },
+            {
+                name = "Toggle logs",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterLogToggle")
+                end,
+            },
+            {
+                name = "Clear Logs",
+                group = "Flutter",
+                execute = function()
+                    vim.cmd("FlutterLogClear")
+                end,
+            },
+        },
+    }
 end
 
 return M
