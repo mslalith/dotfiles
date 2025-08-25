47f048a2208d9a33962065f2c60f7385a49699df
mslalith <mslalith.opensource@gmail.com>
2025-08-25 09:47:05 +0530

    ansible: remove karabiner elements

--- brew.sh
+++ brew.sh
@@ -3,6 +3,3 @@ brew install koekeishiya/formulae/yabai
 brew services start koekeishiya/formulae/yabai
 chmod +x ./yabai/.config/yabai/yabairc
 
-# karabiner-elements
-brew install --cask karabiner-elements
-
